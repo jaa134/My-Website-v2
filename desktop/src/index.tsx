d0adbb2a51566af172fbe9aa12503227f308ad1e
jacob-alspaw <jacob@loftorbital.com>
2023-02-24 00:29:38 -0700

    revert test commit and fix subdomain bouncing

--- desktop/src/index.tsx
+++ desktop/src/index.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
-import { deviceType, isDesktop, isMobile, isMobileOnly, isTablet } from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+
+import App from 'src/components/App';
+import ScrollToTop from 'src/components/common/scrollToTop/ScrollToTop';
 
 import reportWebVitals from 'src/reportWebVitals';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Root element missing in index.html');
+if (process.env.NODE_ENV === 'production' && isMobile) {
+  window.location.replace('https://mobile.jacob-alspaw.com');
+} else {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) {
+    throw new Error('Root element missing in index.html');
+  }
+
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <HashRouter>
+        <ScrollToTop />
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+  );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+  // eslint-disable-next-line no-console
+  reportWebVitals(console.log);
 }
-
-const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <div>
-      <div>isMobile: {isMobile ? 'yes' : 'no'}</div>
-      <div>isMobileOnly: {isMobileOnly ? 'yes' : 'no'}</div>
-      <div>isTablet: {isTablet ? 'yes' : 'no'}</div>
-      <div>isDesktop: {isDesktop ? 'yes' : 'no'}</div>
-      <div>deviceType: {deviceType}</div>
-    </div>
-  </React.StrictMode>,
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// eslint-disable-next-line no-console
-reportWebVitals(console.log);
--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { isDesktop } from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
@@ -8,7 +8,7 @@ import ScrollToTop from 'src/components/common/scrollToTop/ScrollToTop';
 
 import reportWebVitals from 'src/reportWebVitals';
 
-if (process.env.NODE_ENV === 'production' && isDesktop) {
+if (process.env.NODE_ENV === 'production' && !isMobile) {
   window.location.replace('https://jacob-alspaw.com');
 } else {
   const rootElement = document.getElementById('root');
